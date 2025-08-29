cfe791527439b21f787e72b1a7308b950e2a77e8
Muhammad Hammad <muhammadhammad20004@gmail.com>
2025-08-29 20:47:32 +0800

    admin files added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,12 @@ import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
 import Blog from './pages/Blog'
+import Dashboard from './pages/admin/dashboard'
+import Layout from './pages/admin/Layout'
+import AddBlog from './pages/admin/AddBlog'
+import ListBlog from './pages/admin/ListBlog'
+import Comment from './pages/admin/Comment'
+import Login from './components/admin/Login'
 
 const App = () => {
   return (
@@ -9,6 +15,15 @@ const App = () => {
     <Routes>
       <Route path='/' element={ <Home /> } />
       <Route path='/blog/:_id' element={ <Blog /> } />
+
+      <Route path='/admin' element={ true ? <Layout /> : <Login /> }>
+        <Route index  element={ <Dashboard /> } />
+        <Route path='add-blog' element={ <AddBlog /> } />
+        <Route path='list-blog' element={ <ListBlog /> } />
+        <Route path='comments' element={ <Comment /> } />
+      </Route>
+
+
       </Routes>  
     </>
   )
---
+++ client/src/pages/admin/AddBlog.jsx
@@ -0,0 +1,29 @@
+import { useState } from "react"
+import { assets } from "../../assets/assets"
+
+const AddBlog = () => {
+
+  const [image,setImage] = useState(false)
+  const [title,setTitle] = useState("")
+  const [subTitle,setSubTitle] = useState("")
+  const [category,setCategory] = useState("Startup")
+  const [isPublished,setIsPublished] = useState(false)
+
+  const onSubmitHandler = async (e) => {
+    e.preventDefault()
+  }
+
+  return (
+   <form onSubmit={onSubmitHandler} className="flex-1 bg-blue-50/50 text-gray-600 h-full overflow-scroll">
+    <div className="bg-white w-full max-w-3xl p-4 md:p-10 sm:m-10 shadow rounded">
+       <p>Upload thumbnail</p>
+       <label htmlFor="image">
+        <img src={assets.upload_area} className="mt-2 h-16 rounded cursor-pointer" alt="" />
+        <input type="file" id="image" hidden required/>
+       </label>
+    </div>
+   </form>
+  )
+}
+
+export default AddBlog
