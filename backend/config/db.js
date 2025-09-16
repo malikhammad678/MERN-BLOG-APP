f5a04eb9209f2e2137c11b1315f3deba609c333f
Muhammad Hammad <muhammadhammad20004@gmail.com>
2025-09-16 22:29:45 +0800

    db changes

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,11 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.DB_URL)
+        await mongoose.connect(process.env.DB_URL,{
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            serverSelectionTimeoutMS: 30000,
+        })
         console.log("MONGODB Connected!")
     } catch (error) {
         console.log(error)
